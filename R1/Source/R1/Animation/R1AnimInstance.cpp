d8520327b530292719a1e7ea8faa1a7aff5776e5
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-06-10 16:16:51 +0900

    Animation 블루프린트

---
+++ R1/Source/R1/Animation/R1AnimInstance.cpp
@@ -0,0 +1,41 @@
+
+
+
+#include "Animation/R1AnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Character/R1Player.h"
+
+UR1AnimInstance::UR1AnimInstance(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+}
+
+void UR1AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = Cast<AR1Player>(TryGetPawnOwner());
+
+	if (Character)
+		MovementComponent = Character->GetCharacterMovement();
+}
+
+void UR1AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (Character == nullptr)
+		return;
+
+	if (MovementComponent == nullptr)
+		return;
+
+	Velocity = MovementComponent->Velocity;
+	GroundSpeed = Velocity.Size2D();
+
+	bShouldMove = (GroundSpeed > 3.f && MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector);
+	bIsFalling = MovementComponent->IsFalling();
+
+}
---
+++ R1/enc_temp_folder/3f2fa7d08fff2fc4249538d33c44c6a/R1AnimInstance.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include "Animation/R1AnimInstance.h"
+
+UR1AnimInstance::UR1AnimInstance(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+}
+
+void UR1AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+
+}
+
+void UR1AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+
+}
