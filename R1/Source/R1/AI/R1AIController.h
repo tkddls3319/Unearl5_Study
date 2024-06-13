e184fee283d1fa167d46d96b50bacf8cac76a9d9
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-06-13 09:52:21 +0900

    AIController 사용해보기 추후 Behavio Tree로 변경

---
+++ R1/Source/R1/AI/R1AIController.h
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "R1AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API AR1AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+		AR1AIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+		virtual void OnPossess(APawn* InPawn)override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
---
+++ R1/enc_temp_folder/b1903f6ed0b9da4d13f5e4f9511296/R1AIController.h
@@ -0,0 +1,21 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "R1AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API AR1AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+		AR1AIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+		virtual void OnPossess(APawn* InPawn)override;
+};
